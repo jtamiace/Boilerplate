ca3e0151faacc7d34a8c446df451acf250ce9956
jtamiace <jtamiace@gmail.com>
2014-04-15 11:47:30 -0700

    AUTH WORKS

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 //dependencies for each module used
+var express = require('express')
+ , graph = require('fbgraph')
+ , app = module.exports = express.createServer();
+ var conf = {
+ 	client_id: '616452641773191'
+ ,	client_secret: 'ef64c9bd218a24b5bd83cf67bcb39bd3'
+ ,	scope: 'email, user_about_me, user_birthday, user_location, publish_stream'
+ ,	redirect_uri: 'http://localhost:3000/auth/facebook'
+ };
+var express = require('express');
 var dotenv = require('dotenv');
 dotenv.load();
-var express = require('express');
-var graph = require('fbgraph');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
@@ -23,8 +31,71 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
 
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
 //routes
 app.get('/', index.view);
+
+app.get('/', function(req, res){
+  res.render("index", { title: "click link to connect" });
+});
+
+app.get('/auth/facebook', function(req, res) {
+
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+    var authUrl = graph.getOauthUrl({
+        "client_id":     conf.client_id
+      , "redirect_uri":  conf.redirect_uri
+      , "scope":         conf.scope
+    });
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+    return;
+  }
+
+  // code is set
+  // we'll send that and get the access token
+  graph.authorize({
+      "client_id":      conf.client_id
+    , "redirect_uri":   conf.redirect_uri
+    , "client_secret":  conf.client_secret
+    , "code":           req.query.code
+  }, function (err, facebookRes) {
+    res.redirect('/UserHasLoggedIn');
+  });
+
+
+});
+
+
+// user gets sent here after being authorized
+app.get('/UserHasLoggedIn', function(req, res) {
+  res.render("index", { title: "Logged In" });
+});
+
+console.log('hi');
+
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
@@ -32,3 +103,5 @@ http.createServer(app).listen(app.get('port'), function(){
 });
 
 
+ 
+ 
